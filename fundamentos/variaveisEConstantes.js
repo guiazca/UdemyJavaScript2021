89ab57127e84cbf81d1e9787dc10eb200d37c5aa
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:39:14 +0000

    Seção 4 Javascript Fundamentos O Básico de Var, let e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+var a = 3 // Nomear de forma certa, estude cuidado
+let b = 4 
+
+var a = 30 //posso redeclarar uma variavel "var"
+// let b = 40 - não posso redeclarar  variável com let
+b = 40
+console.log(a , b)
+
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5
+// c = 50 -> Constante não muda!!!
+
+console.log(c)
