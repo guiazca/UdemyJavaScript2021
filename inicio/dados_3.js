3b3a7ac8f01f536b6b7b6897e5a38302b226a626
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:14:43 +0000

    Seção 03 Iniciando com Programação Trabalhando com Dados #03

---
+++ inicio/dados_3.js
@@ -0,0 +1,16 @@
+let idade = 31
+console.log (typeof 31)
+console.log (typeof -31)
+console.log(typeof idade)
+
+let salario = 4578.32
+salario = 45.7832
+salario = 0.457832 //Ponto Flutuante
+console.log(typeof salario)
+
+let estaChovendo = true
+estaChovendo = false
+console.log(typeof estaChovendo)
+
+console.log(typeof "teste")
+console.log(typeof 'teste')
