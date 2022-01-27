3b7564d29900a3e404e9501efa45a9792dacb527
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:12:08 +0000

    Seção 03 Iniciando com Programação Desafio Sentença e Bloco

---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,50 @@
+/*
+    // Bloco 1
+    1
+    3
+    5
+    7
+    9
+
+    //Bloco 2
+    2
+    4
+    6
+    8
+    10 
+*/
+
+{
+    console.log("Bloco 1 - Números Ímpares")
+    console.log("Número 1")
+    console.log("Número 3")
+    console.log("Número 5")
+    console.log("Número 7")
+    console.log("Número 9")
+}
+
+{
+    console.log("Bloco 1 - Números Ímpares")
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+}
+
+{
+    console.log("Bloco 2 - Números Pares")
+    console.log("Número 2")
+    console.log("Número 4")
+    console.log("Número 6")
+    console.log("Número 8")
+    console.log("Número 10")
+}
+{
+    console.log("Bloco 1 - Números Ímpares")
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
