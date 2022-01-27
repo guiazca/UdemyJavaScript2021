bca6aad0692359bcf512f7f5c3cc5acb2dee823c
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:10:56 +0000

    Seção 03 Iniciando com Programação Bloco de Código

---
+++ inicio/bloco.js
@@ -0,0 +1,28 @@
+{ //Inicio do Bloco de Código
+    console.log("Passo #01") // Sentença de Código
+    console.log("Passo #02")
+    console.log("Passo #03")
+} //Fim do Bloco de Código
+
+{
+    console.log("Passo #04")
+    console.log("Passo #05")
+}
+
+{
+
+
+
+}
+
+{
+    {
+        {
+            {
+                console.log("Bloco dentro de Bloco!!")
+            }
+        }
+    }
+}
+
+console.log("fim!")
