9ae1203b17e7c62491b786b23474b28e9933086d
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:07:32 +0000

    secao03 Iniciando com Programação Sentença de Código 02

---
+++ inicio/sentenca_2.js
@@ -0,0 +1,11 @@
+/*Soma dois numeros*/
+console.log(1+3)
+console.log("teste - aspas duplas")
+console.log('teste - aspas simples')
+console.log("Passo #01")
+console.log("Passo #02")
+console.log("Passo #03")
+/* Modificando as ordens do Passo */
+console.log("Passo #03")
+console.log("Passo #01")
+console.log("Passo #02")
