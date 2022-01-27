91e5618c09c0ccd618c3643656d92655ac261f6f
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:37:25 +0000

    Seção 4 Javascript Fundamentos Comentários de Código

---
+++ fundamentos/comentario.js
@@ -0,0 +1,29 @@
+//Comentário de uma linha
+console.log('Linha 1')
+
+/*
+    Comentário de 
+    múltiplas linhas
+*/
+console.log('Linha 2')
+
+/* 
+*  Comentário de
+*  múltiplas Linhas
+*/ 
+console.log('Linha 3')
+
+/*
+    1 - Evite comentários Irrelevantes
+    ex: Nome da função - fecharPedido -
+        - Essa funcionário fechar pedido
+    2 - Comentário relevante
+        - Essa funcionalidade é complexa, pois 
+        interage com vários outros sistemas, 
+        sistema de correios, estoque, caso exista um erro
+        Tratar de tal forma.
+    3 -  Prefêrencia ao código legival, linguagem bíqua
+        http://www.macoratti.net/11/05/ddd_liv1.htm
+        Nomes que todos conheces.
+    4 - Preze pelo seu código, claro e limpo
+*/
