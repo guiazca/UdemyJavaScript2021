5d531c96ce6edc0263741b8c572f90b124ba2b4b
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:42:11 +0000

    Seção 4 Javascript Fundamentos Number: Alguns Cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+console.log (7 / 0)
+console.log("10" / 2) //Ele resolve o 10 como numero
+console.log("3" + 2) //Concatena resultado 32
+console.log("3" - 2) // Ele subtrai pois "-" não faz sentido concatenar
+console.log("10,2" / 2)
+console.log("10.2" / 2)
+console.log("show!" * 2)
+console.log(0.1 + 0.7) 
+//  IEEE Standard for Binary Floating-Point Arithmetic
+// https://www.ecma-international.org/wp-content/uploads/ECMA-262_1st_edition_june_1997.pdf
+// console.log(10.toString()) erro, não consegue fazer com numero literal
+console.log(10.345.toFixed(2))
