ac506c5ac0ba1ad4054ccf029f8495e63b97d075
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:12:58 +0000

    Seção 03 Iniciando com Programação Trabalhando com Dados #01

---
+++ inicio/dados_1.js
@@ -0,0 +1,35 @@
+var nome ="Caneta" // Declaração de Variavel
+var quantidade = 10
+var preco = 6.4
+var imposto = 1.5
+var precoFinal = preco + imposto
+console.log(nome) //string
+console.log(quantidade) // valor literal
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = "Caneta BIC" //Alterei a variavel
+console.log(nome)
+
+blabla = 123 //Não é ideal
+console.log(blabla)
+
+let nome ="Caneta" // Declaração de Variavel
+let quantidade = 10
+let preco = 6.4
+let imposto = 1.5
+let precoFinal = preco + imposto
+console.log(nome) //string
+console.log(quantidade) // valor literal
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = "Caneta BIC" //Alterei a variavel
+console.log(nome)
+
+blabla = 123 //Não é ideal
+console.log(blabla)
+
+
