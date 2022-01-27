7167505b5c5a73e0e27577a768785f89161702e5
Guilherme Carvalho <guiazca@gmail.com>
2022-01-27 17:14:06 +0000

    Seção 03 Iniciando com Programação Trabalhando com Dados #02

---
+++ inicio/dados_2.js
@@ -0,0 +1,28 @@
+console.log(19.9 * 0.6)
+
+let preco = 19.90
+let desconto = 0.4
+
+console.log(preco - (preco*desconto))
+console.log(preco * (1 - desconto))
+
+let  precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto)
+
+let nome = "Caderno" //string(Texto) -> Sequencia de símnbolos
+let categoria = "Papelaria"
+console.log(nome+categoria)
+
+nome = "Caderno "
+categoria = "Papelaria"
+console.log(nome+categoria)
+
+nome = "Caderno"
+categoria = "Papelaria"
+console.log("Produto: "+ nome
++ ", Categoria: "+ categoria 
++", Preço: "+ preco
++", Desconto: "+ desconto)
+
+console.log(1 + 1)
+console.log("1" + 1)
