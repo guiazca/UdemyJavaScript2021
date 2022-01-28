d8e1741f02726ab567395a123f08b14d5e4aaf60
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 11:52:34 +0000

    Seção 8 JavaScript: Array Concat

---
+++ array/concat.js
@@ -0,0 +1,11 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos)
+console.log(todos)
+console.log(filhas)
+console.log(filhos)
+const todos2 = filhas.concat(filhos, 'Fulano')
+console.log(todos2)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7]]))
+console.log(['a', 'b'].concat([1,2], [3,4], 5, [[6,7]]))
