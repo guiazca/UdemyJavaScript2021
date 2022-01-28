c3bf6fe2a1d974a29cdd6a7746d8d1e4627ddd77
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 11:04:51 +0000

    Seção 8 JavaScript: Array Map #02

---
+++ array/map2.js
@@ -0,0 +1,28 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+// Retornar um array apenas com os preços
+
+const parse = e => JSON.parse(e)
+const getPreco = e => e.preco
+
+const preco = carrinho.map(parse).map(getPreco)
+console.log(preco)
+
+// SOLUCAO PROFESSOR
+const carrinho2 = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho2.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
