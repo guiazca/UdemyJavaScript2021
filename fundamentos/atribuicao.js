a7ee6cb6220254bc245d1c97eebbd11b4318f1f3
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 06:25:53 +0000

    Seção 4 Javascript Fundamentos Operadores: Atribuição

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 //colocando o valor 7 dentro de a
+let b = 3
+
+b += a // b = b + a
+console.log(b)
+
+b -= 4 //b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2 -> resto da divisão de b por 2
+console.log(b)
