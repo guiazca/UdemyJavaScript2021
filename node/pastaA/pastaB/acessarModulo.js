3fe12492a683805e42c6124a4bc1d58b1c12bfea
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 14:40:14 +0000

    Seção 10 Node Sistema de Módulos: Require

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+// Leia as Mensagens!!!!!!! de ERROO!!!!!
+
+
+const c = require('./pastaC') //não preciso referenciar o arquivo index.js
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia')
+//     res.end()
+// }).listen(8080) //Olhar no browser http://localhost:8080
