0d8fa78d07f6e026ab6fa9257a412b72efa4fc98
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 06:36:25 +0000

    Seção 4 Javascript Fundamentos Operadores: Ternários

---
+++ fundamentos/ternario.js
@@ -0,0 +1,8 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+//Operador ternario
+// Parte 1 , retorna verdadeiro ou false
+// Parte 2 , resposta do valor verdadeiro
+// Parte 3 , resposta do valor falso
