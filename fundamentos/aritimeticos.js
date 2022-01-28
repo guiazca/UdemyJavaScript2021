e09e11afb7bf5f2a66ff86cd9275d25ec12c58c8
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 06:30:26 +0000

    Seção 4 Javascript Fundamentos Operadores: Aritméticos

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,18 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 //Resto da divisao
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
+console.log(soma, subtracao, multiplicacao, divisao, modulo )
+ // b++ -> post fix
+ // ++b ->  pre fix
+ // a + b -> inn fix
+ // operadores binários, 2 operando
+
