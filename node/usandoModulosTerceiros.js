ec3dc49aa833dbc9cf8bd28ee8cf62add989e823
Guilherme Carvalho <guiazca@gmail.com>
2022-01-28 14:35:30 +0000

    Seção 10 Node Usando Módulos de Terceiros

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,6 @@
+const _= require('lodash')
+// Gerar um valor aleatorio entre 1 e 1000 de 2 em 2 segundos
+setInterval(() => console.log(_.random(1, 100)), 2000)
+
+// Instalado dependencia lodash
+// Instalado dependencia nodemon -> Live server do node
